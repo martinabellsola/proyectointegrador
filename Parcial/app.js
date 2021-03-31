787c5cb88f4da06a38b59a670a5ab0691a3ec1b2
martinabellsola <mbellsolaferrer@udesa.edu.ar>
2021-03-31 12:08:07 -0300

    cambios en rutas

--- Parcial/app.js
+++ Parcial/app.js
@@ -8,6 +8,7 @@ var indexRouter = require("./routes/index")
 var loginRouter = require("./routes/login")
 var registrationRouter = require("./routes/registration")
 var searchRouter = require("./routes/search")
+var profileRouter = require("./routes/profile")
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/' , indexRouter)
 app.use('/login' , loginRouter)
 app.use('/registration' , registrationRouter)
 app.use('/search' , searchRouter)
+app.use('/profile' , profileRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
