39a88de606f10dce82e54bb489d836f10b32c43d
carlosjescobar <cescobar@udesa.edu.ar>
2021-04-19 21:36:33 -0300

    fotos usuarios+formularios registro y login

--- Parcial/data/users.js
+++ Parcial/data/users.js
@@ -3,37 +3,43 @@ let usuario = [
     cantidadComentarios: "8",
     cantidadProductos: "300",
     cantidadSeguidores:"1200",
-    idUsuario: 1
+    idUsuario: 1,
+    urlFoto:"/images/footer/fotoJuan.jpg"
     },
     {usuario: "Felipe Gonzalez",
     cantidadComentarios: "2",
     cantidadProductos: "700",
     cantidadSeguidores:"500",
-    idUsuario: 2
+    idUsuario: 2,
+    urlFoto:"/images/footer/fotoFelipe.jpg"
     },
     {usuario: "Miguel Gomez",
     cantidadComentarios: "1",
     cantidadProductos: "10",
     cantidadSeguidores:"300",
-    idUsuario: 3
+    idUsuario: 3,
+    urlFoto:"/images/footer/fotoMiguel.jpg"
     },
     {usuario: "Maia Sanchez",
     cantidadComentarios: "12",
     cantidadProductos: "170",
     cantidadSeguidores:"125",
-    idUsuario: 4
+    idUsuario: 4,
+    urlFoto:"/images/footer/fotoMaia.jpg"
     },
     {usuario: "Camila Pochettino",
     cantidadComentarios: "18",
     cantidadProductos: "250",
     cantidadSeguidores:"780",
-    idUsuario: 5
+    idUsuario: 5,
+    urlFoto:"/images/footer/fotoCamila.jpg"
     },
     {usuario: "Veronica Rizzotti", 
     cantidadComentarios: "7",
     cantidadProductos: "400",
     cantidadSeguidores:"1500",
-    idUsuario: 6
+    idUsuario: 6,
+    urlFoto:"/images/footer/fotoVeronica.jpg"
     },
 ]
 
