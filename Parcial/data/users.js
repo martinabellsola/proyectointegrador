202f8d25c24da8da0ceec04fbd8da82a6aee2976
martinabellsola <mbellsolaferrer@udesa.edu.ar>
2021-04-21 11:05:22 -0300

    Orden y cambios

--- Parcial/data/users.js
+++ Parcial/data/users.js
@@ -4,42 +4,42 @@ let usuario = [
     cantidadProductos: "300",
     cantidadSeguidores:"1200",
     idUsuario: 1,
-    urlFoto:"/images/footer/fotoJuan.jpg"
+    urlFoto:"/images/usuarios/fotoJuan.jpg"
     },
     {usuario: "Felipe Gonzalez",
     cantidadComentarios: "2",
     cantidadProductos: "700",
     cantidadSeguidores:"500",
     idUsuario: 2,
-    urlFoto:"/images/footer/fotoFelipe.jpg"
+    urlFoto:"/images/usuarios/fotoFelipe.jpg"
     },
     {usuario: "Miguel Gomez",
     cantidadComentarios: "1",
     cantidadProductos: "10",
     cantidadSeguidores:"300",
     idUsuario: 3,
-    urlFoto:"/images/footer/fotoMiguel.jpg"
+    urlFoto:"/images/usuarios/fotoMiguel.jpg"
     },
     {usuario: "Maia Sanchez",
     cantidadComentarios: "12",
     cantidadProductos: "170",
     cantidadSeguidores:"125",
     idUsuario: 4,
-    urlFoto:"/images/footer/fotoMaia.jpg"
+    urlFoto:"/images/usuarios/fotoMaia.jpg"
     },
     {usuario: "Camila Pochettino",
     cantidadComentarios: "18",
     cantidadProductos: "250",
     cantidadSeguidores:"780",
     idUsuario: 5,
-    urlFoto:"/images/footer/fotoCamila.jpg"
+    urlFoto:"/images/usuarios/fotoCamila.jpg"
     },
     {usuario: "Veronica Rizzotti", 
     cantidadComentarios: "7",
     cantidadProductos: "400",
     cantidadSeguidores:"1500",
     idUsuario: 6,
-    urlFoto:"/images/footer/fotoVeronica.jpg"
+    urlFoto:"/images/usuarios/fotoVeronica.jpg"
     },
 ]
 
