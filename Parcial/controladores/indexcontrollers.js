8529950995e8cc0b58a7f463e941dbe0949eaf1c
CamilaBarbareschi <camilabarbareschi@outlook.com>
2021-05-14 17:14:28 -0300

    + productos

--- Parcial/controladores/indexcontrollers.js
+++ Parcial/controladores/indexcontrollers.js
@@ -1,23 +1,26 @@
-const products = require("../data/products")
 let db = require('../database/models')
 const Op = db.Sequelize.Op;
 
 const controlador = {
  index: (req, res, next)=>{
-   db.Producto.findAll().then(products=>{
+   let filtro = {
+      order: [
+         ['fechaCreacion', 'DESC'],
+      ],
+   }     //habría que crear un nuevo filtro para las más comentadas
+   db.Producto.findAll(filtro).then(products=>{
       res.render("index", { products: products})
-   })
+   }).catch(err => {console.log(err)})
  },
  producto: (req, res, next)=>{
-    db.Producto.findByPk(req.params.id).then(products=>{
-          res.render("product", {products:products,})
-        }
-    
-    )
-    
+   db.Producto.findByPk(req.params.id).then(products=>{
+      res.render("product", {products:products,})
+   }).catch(err => {console.log(err)})
  },
  productoAdd: (req, res, next)=>{
-    res.render("product-add", { products: products} )
+   db.Producto.findAll().then(products=>{
+      res.render("product-add", { products: products} )
+   }).catch(err => {console.log(err)})
  },
  productoCrear:(req, res, next)=>{
     db.Producto.create({
@@ -25,12 +28,9 @@ const controlador = {
        imagen : req.body.imagen,
        descripcion: req.body.descripcion,
        fechaCreacion: req.body.fecha
-
-   }).then(
-      productocreado=>{
+      }).then( productocreado=>{
          res.redirect('../product/'+productocreado.id)
-      }
-   ).catch(err => {console.log(err)})
+      }).catch(err => {console.log(err)})
 }}
 
 module.exports = controlador
--- Parcial/database/config/config.js
+++ Parcial/database/config/config.js
@@ -1,9 +1,8 @@
 module.exports = {
    "development": {
     "username": "root",
-    "password": "root",
-    "port":"8889",
-    "database": "tpp2",
+    "password": null,
+    "database": "hooves",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
