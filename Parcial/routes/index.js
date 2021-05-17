ea87cbe8788de1a1071582510b0a884a9b33b806
CamilaBarbareschi <camilabarbareschi@outlook.com>
2021-05-17 09:26:14 -0300

    multer

--- Parcial/routes/index.js
+++ Parcial/routes/index.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
 let controlador = require("../controladores/indexControllers")
 
+//MULTER 
+var multer = require("multer");
+var upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "./public/images/products");
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname + "-" + Date.now())
+        }
+    }),
+})
 
 router.get ('/', controlador.index)
 router.get ('/product/:id', controlador.producto)
 router.get ('/add', controlador.productoAdd)
-router.post('/crear', controlador.productoCrear)
-
+router.post("/crear", upload.single("imagen"), controlador.productoCrear)
 module.exports = router
